5e8239be316d89284212a1ff00578a91e7e8a8e0
Barel Shraga <barel123133@gmail.com>
2023-01-30 00:36:39 +0200

    Added auth routes

--- src/router.ts
+++ src/router.ts
@@ -1,24 +1,34 @@
-import homePage from './views/home-page.vue'
-import resumeApp from './views/resume-app.vue'
-import signUp from './views/sign-up.vue'
+import homePage from './views/home-page.vue';
+import resumeApp from './views/resume-app.vue';
+import SignUpPage from './views/auth/signup-page.vue';
+import loginPage from './views/auth/login-page.vue';
+import forgetPassword from './views/auth/forget-password-page.vue';
 
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router';
 const routes = [
-    {
-        path: '/',
-        component: homePage
-    },
-    {
-        path: '/resume/:id',
-        component: resumeApp,
-    },
-    {
-        path: '/signup',
-        component: signUp
-    },
-]
+	{
+		path: '/',
+		component: homePage,
+	},
+	{
+		path: '/resume/:id',
+		component: resumeApp,
+	},
+	{
+		path: '/signup',
+		component: SignUpPage,
+	},
+	{
+		path: '/login',
+		component: loginPage,
+	},
+	{
+		path: '/forget-password',
+		component: forgetPassword,
+	},
+];
 
 export const router = createRouter({
-    history: createWebHashHistory(),
-    routes,
-})
+	history: createWebHashHistory(),
+	routes,
+});
