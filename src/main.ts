fa4d7b530ea7b481a9b731609203efe2e36145f0
Barel Shraga <barel123133@gmail.com>
2023-01-30 02:55:58 +0200

    Implement login page

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import { createApp } from 'vue'
-import './assets/scss/global.scss'
-import App from './App.vue'
-import { router } from './router'
+import './assets/scss/global.scss';
+
+import { createApp } from 'vue';
+import router from './router';
 import { createPinia } from 'pinia';
+import App from './App.vue';
+import { OhVueIcon, addIcons } from 'oh-vue-icons';
+import { MdEmail, RiLockLine } from 'oh-vue-icons/icons';
 
 const pinia = createPinia();
 
-const app = createApp(App)
+addIcons(MdEmail, RiLockLine);
+
+const app = createApp(App);
 app.use(pinia);
-app.use(router)
-app.mount('#app')
+app.use(router);
+app.component('v-icon', OhVueIcon);
+app.mount('#app');
--- src/views/auth/login-page.vue
+++ src/views/auth/login-page.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="login-page">
-		<div class="header">
+		<div class="login-header">
 			<router-link to="/" class="logo">
 				<logo-svg2 />
 			</router-link>
@@ -8,26 +8,53 @@
 
 		<form class="login-container">
 			<h1>Login</h1>
-			<div>
-				<!-- Email icon here -->
-				<input type="email" name="email" placeholder="Email" />
-			</div>
-			<div>
-				<!-- Password icon here -->
-				<input type="password" name="password" placeholder="Password" />
-			</div>
+
+			<CFormFloating class="mb-3">
+				<CFormInput type="email" placeholder="Email" />
+				<CFormLabel for="floatingInput"
+					><span
+						><v-icon name="md-email" /> Email address</span
+					></CFormLabel
+				>
+			</CFormFloating>
+
+			<CFormFloating class="mb-3">
+				<CFormInput
+					:type="showPassword ? 'text' : 'password'"
+					placeholder="Password" />
+				<CFormLabel for="floatingInput">
+					<span
+						><v-icon name="ri-lock-line" /> Password</span
+					></CFormLabel
+				>
+			</CFormFloating>
+
+			<button @click="showPassword = !showPassword">
+				{{ showPassword ? 'Show' : 'Hide' }}
+			</button>
+
 			<router-link to="/forget-password">Forget password?</router-link>
+
 			<button type="submit">Login</button>
 		</form>
 	</div>
 </template>
 
 <script lang="ts">
+import { CFormFloating, CFormInput, CFormLabel } from '@coreui/vue';
 import logoSvg2 from '../../components/logo-svg2.vue';
 
 export default {
 	components: {
 		logoSvg2,
+		CFormFloating,
+		CFormInput,
+		CFormLabel,
+	},
+	data() {
+		return {
+			showPassword: false,
+		};
 	},
 };
 </script>
